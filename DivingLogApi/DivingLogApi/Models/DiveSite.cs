a02d90fe93baf73d3c9a16ad4dfcba10bb0e2071
Marcin <mieker@wp.pl>
2020-11-17 19:58:18 +0100

    gitignore and raw model added enums included

---
+++ DivingLogApi/DivingLogApi/Models/DiveSite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivingLogApi.Models
+{
+    public class DiveSite
+    {
+        public int DiveSiteId { get; set; }
+        public string Name { get; set; }
+        public string GPSPosition { get; set; }
+        public string Description { get; set; }
+        public List<Dive> Dives { get; set; }
+
+    }
+}
---
+++ DivingLogApi/DivingLogApi/Models/Enums/Water.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivingLogApi.Models.Enums
+{
+    public enum Water
+    {
+        Słona,
+        Słodka
+    }
+}
