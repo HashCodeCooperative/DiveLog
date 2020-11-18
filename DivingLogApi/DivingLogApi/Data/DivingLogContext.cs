003948d5dae109d6e140d21c2e9d0fa4a081e9ca
Marcin <mieker@wp.pl>
2020-11-18 18:47:31 +0100

    sqlite database with libs and context added

---
+++ DivingLogApi/DivingLogApi/Data/DivingLogContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivingLogApi.Data
+{
+    public class DivingLogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=Data/DivingLogDb.db");
+    }
+}
